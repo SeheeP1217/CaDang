d402ec292a0688ff5a7166a652d3066b1ec79794
정재영 <jaywjd@naver.com>
2023-01-26 14:51:21 +0900

    [BE] Entity 생성

--- BE/cadang/src/main/java/com/ssafy/cadang/domain/User.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/domain/User.java
@@ -1,15 +1,42 @@
 package com.ssafy.cadang.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "user")
 public class User {
 
+    @JsonIgnore
     @Id
     @GeneratedValue
     private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private String nickname;
+
+    private String img;
+
+    private Long caffeGoal;
+
+    private Long sugarGoal;
+
+    private Timestamp lastUpdated;
+
+    private Long authId;
+
+    private String refreshToken;
+
+    @ManyToOne
+    @JoinColumn(name = "auth_id")
+    private Authority authority;
+
+
 }
