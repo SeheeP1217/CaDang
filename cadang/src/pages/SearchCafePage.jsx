dc1c973ff97e103786ed74b0746757148d6e0e9b
chloe73 <esom735@gmail.com>
2023-01-27 01:45:34 +0900

    [FE] feat: 카카오지도 띄우기

---
+++ cadang/src/pages/SearchCafePage.jsx
@@ -0,0 +1,41 @@
+// import * as React from "react";
+import React, { useRef, useEffect } from "react";
+
+const { kakao } = window;
+
+export default function SearchCafePage() {
+  
+  useEffect(() => {
+    let markers = [];
+    const container = document.getElementById("map"); // 지도를 표시할 div
+
+    const options = {
+        center: new window.kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 9, // 지도의 확대 레벨
+    };
+
+    const map = new window.kakao.maps.Map(container, options);
+
+    const markerPosition  = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488);
+    const marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+    marker.setMap(map);
+
+    // 버튼 클릭에 따라 지도 이동 기능을 막거나 풀고 싶은 경우에는 map.setDraggable 함수를 사용
+    function setDraggable(draggable) {
+        // 마우스 드래그로 지도 이동 가능여부를 설정
+        map.setDraggable(draggable);
+    }
+
+    console.log("loading kakaomap");
+}, []);
+
+  return (
+    <div
+      id="map"
+      className="map"
+      style={{ width: "500px", height: "500px" }}
+    ></div>
+  );
+}
