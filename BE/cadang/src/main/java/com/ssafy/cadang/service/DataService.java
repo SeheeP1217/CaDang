28c34f72b569c6463e02aad93d68302e365951c1
YunGyung Lee <yxxnkxx@gmail.com>
2023-01-29 20:38:50 +0900

    [BE] Data 등록 구현

--- BE/cadang/src/main/java/com/ssafy/cadang/service/DataService.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/service/DataService.java
@@ -1,20 +1,35 @@
 package com.ssafy.cadang.service;
 
 import com.ssafy.cadang.domain.Data;
+import com.ssafy.cadang.domain.User;
 import com.ssafy.cadang.repository.DataRepository;
+import com.ssafy.cadang.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DataService {
     private final DataRepository dataRepository;
+    private final UserRepository userRepository;
 
 
-    public void save(Data d) {
-        dataRepository.save(d);
+    /**
+     * 회원가입시 자동으로 오늘 data 만들기`
+     */
+    @Transactional
+    public Long createData(Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        User user = userOptional.orElse(null);
+        if (user != null) {
+            Data saveData = dataRepository.save(new Data(user));
+            return saveData.getId();
+        }
+        return null; // 에러 처리 하기
     }
 
 }
--- BE/cadang/src/test/java/com/ssafy/cadang/domain/DataTest.java
+++ BE/cadang/src/test/java/com/ssafy/cadang/domain/DataTest.java
@@ -1,15 +1,18 @@
 package com.ssafy.cadang.domain;
 
 import com.ssafy.cadang.repository.DataRepository;
-import com.ssafy.cadang.repository.UserRepository;
+import com.ssafy.cadang.service.DataService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import java.util.List;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -17,23 +20,23 @@ import java.util.List;
 class DataTest {
 
     @Autowired
-    private DataRepository dataRepository;
+    private DataService dataService;
     @Autowired
-    private UserRepository userRepository;
+    private DataRepository dataRepository;
+    @PersistenceContext
+    private EntityManager em;
 
 
     @Test
-    public void test() {
+    public void dataCreateTest() {
 
-        List<User> all = userRepository.findAll();
+        Long save = dataService.createData(1L);
+        em.flush();
+        em.clear();
+        Optional<Data> byId = dataRepository.findById(save);
 
-        User user = all.get(0);
-        System.out.println(user.getUserName());
-        Data data = new Data(user);
-        dataRepository.save(data);
-
-
-
+        assertThat(byId.orElse(null).getId()).isEqualTo(save);
+        assertThat(byId.orElse(null).getRegDate()).isEqualTo(LocalDate.of(2023, 1, 29));
 
     }
 
