4aa0c9fae695ce333a92d9a82ee98177d1fa3cac
YunGyung Lee <yxxnkxx@gmail.com>
2023-01-29 22:13:41 +0900

    [BE] security 설정 끄기

--- BE/cadang/src/main/java/com/ssafy/cadang/config/WebSecurityConfiguration.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/config/WebSecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.ssafy.cadang.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,5 +12,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // …
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
     }
 }
