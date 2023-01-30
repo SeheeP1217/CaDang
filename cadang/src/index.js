a1de5fafea00ed097b9504950a8b9864173d377e
sehee <parksehee6938@gmail.com>
2023-01-30 17:07:46 +0900

    [FE] feat: carousel 수정

--- cadang/src/index.js
+++ cadang/src/index.js
@@ -5,12 +5,12 @@ import { BrowserRouter } from "react-router-dom"
 import "./index.css"
 import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+)
 
 // ReactDOM.render(
 //   <BrowserRouter>
