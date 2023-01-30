a916d78672e1bd2b24eeafe65ceb408ade685485
unknown <gkh4302@naver.com>
2023-01-30 13:21:50 +0900

    feat: OrderController, Service 수정

--- BE/cadang/src/main/java/com/ssafy/cadang/service/OrderService.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ssafy.cadang.service;
 
+import com.ssafy.cadang.dto.OrderDto;
 import com.ssafy.cadang.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,11 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     //git
-    public Long orderRegister(){
+    public Long saveOrder(OrderDto orderDto) {
+
+        //연관관계 있는 필드는 find 후 할당해서 save해야 한다..
+
+        //Dto to Entity 빠르게 하는 방법 없누
 
         return null;
     }
