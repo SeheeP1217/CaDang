7476dd7517e10fe64729cf693a919186265f7a0a
정재영 <jaywjd@naver.com>
2023-01-31 17:54:40 +0900

    [BE]feat: img 업로드 작업중

---
+++ BE/cadang/src/main/java/com/ssafy/cadang/file/FileStore.java
@@ -0,0 +1,47 @@
+package com.ssafy.cadang.file;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename){
+        return fileDir + filename;
+    }
+
+    public UploadFile storefile(MultipartFile multipartFile) throws IOException{
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFilename = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFilename)));
+        return new UploadFile(originalFilename, storeFilename);
+
+    }
+
+    // 서버에 저장할 파일명을 만든다.
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    // 원본 파일의 확장자를 반환한다.
+    private String extractExt(String originalFilename){
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+
+}
---
+++ BE/cadang/src/main/java/com/ssafy/cadang/file/UserProfileImgController.java
@@ -0,0 +1,31 @@
+package com.ssafy.cadang.file;
+
+
+import com.ssafy.cadang.dto.UserDto;
+import com.ssafy.cadang.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class UserProfileImgController {
+
+    private final UserRepository userRepository;
+    private final FileStore fileStore;
+
+
+//    public String saveProfile(@ModelAttribute
+//                              UserDto userDto,
+//                              RedirectAttributes) throws IOException{
+//        UploadFile attachFile = fileStore.storefile(userDto.getImg());
+//
+//
+//    }
+
+}
