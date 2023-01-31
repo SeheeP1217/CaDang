303f32f59d09fccdd41a93e8fedd1863b9c882f4
정재영 <jaywjd@naver.com>
2023-01-31 10:28:24 +0900

    [BE]feat: 비밀번호 암호화

--- BE/cadang/src/main/java/com/ssafy/cadang/service/UserService.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/service/UserService.java
@@ -6,12 +6,10 @@ import com.ssafy.cadang.domain.User;
 
 import com.ssafy.cadang.dto.UserDto;
 import com.ssafy.cadang.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 
 @Service
@@ -21,6 +19,9 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final Long defaultCaffeineGoal = 400L;
+    private final Long defaultSugarGoal = 25L;
+
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
@@ -29,42 +30,44 @@ public class UserService {
     //회원가입
     public void join(UserDto userDto){
 
-        userRepository.existsByMemberId(userDto.getMemberId());
-        userRepository.existsByEmail(userDto.getEmail());
-        userRepository.existsByNickname(userDto.getNickname());
+        validateDuplicateUserId(userDto);
+        validateDuplicateEmail(userDto);
+        validateDuplicateNickname(userDto);
 
         //TODO: activated 필드를 만들어야 되나?
-
         User user = User.builder()
                         .userName(userDto.getUsername())
                         .memberId(userDto.getMemberId())
                         .password(passwordEncoder.encode(userDto.getPassword()))
+                        .email(userDto.getEmail())
                         .nickname(userDto.getNickname())
                         .authorityName("ROLE_USER")
+                        .caffeGoal(defaultCaffeineGoal)
+                        .sugarGoal(defaultSugarGoal)
                         .build();
         userRepository.save(user);
 
     }
 
     @Transactional(readOnly = true)
-    public void validateDuplicateUserId(User user){
-        boolean userIdDuplicate = userRepository.existsByMemberId(user.getMemberId());
+    public void validateDuplicateUserId(UserDto userDto){
+        boolean userIdDuplicate = userRepository.existsByMemberId(userDto.getMemberId());
         if(userIdDuplicate){
             throw new IllegalStateException("이미 존재하는 아이디입니다");
         }
     }
 
     @Transactional(readOnly = true)
-    public void validateDuplicateEmail(User user){
-        boolean userEmailDuplicate = userRepository.existsByEmail(user.getEmail());
+    public void validateDuplicateEmail(UserDto userDto){
+        boolean userEmailDuplicate = userRepository.existsByEmail(userDto.getEmail());
         if(userEmailDuplicate){
             throw new IllegalStateException("이미 존재하는 이메일입니다");
         }
     }
 
     @Transactional(readOnly = true)
-    public void validateDuplicateNickname(User user){
-        boolean userNicknameDuplicate = userRepository.existsByNickname(user.getNickname());
+    public void validateDuplicateNickname(UserDto userDto){
+        boolean userNicknameDuplicate = userRepository.existsByNickname(userDto.getNickname());
         if(userNicknameDuplicate){
             throw new IllegalStateException("이미 존재하는 닉네임입니다");
         }
