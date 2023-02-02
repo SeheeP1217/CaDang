4ba38672a37b156d368e126aed1be441d4d08fc5
이윤경 <yxxnkxx@gmail.com>
2023-02-02 10:32:37 +0900

    [BE] 에러 처리,일간 데이터 수정

--- BE/cadang/src/main/java/com/ssafy/cadang/error/ExceptionEnum.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/error/ExceptionEnum.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ExceptionEnum {
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+
+    DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "데이터를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
