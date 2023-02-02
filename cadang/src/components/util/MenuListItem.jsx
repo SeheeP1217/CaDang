cd408315c0ff9e687e15aba2cf5f513d7ca60f54
임소정 <sjjj.lim@gmail.com>
2023-02-02 10:47:23 +0900

    feat: 카드 컴포넌트 추가

--- cadang/src/components/util/MenuListItem.jsx
+++ cadang/src/components/util/MenuListItem.jsx
@@ -1,27 +1,36 @@
 import * as React from "react";
-import { Paper, Grid } from "@mui/material";
+import { Paper, Grid, Divider } from "@mui/material";
 import Typography from "@mui/joy/Typography";
-import drink from '../../assets/drink.png';
+import drink from "../../assets/drink.png";
 import { styled } from "@mui/material/styles";
 
 function MenuListItem(props) {
   return (
-    <Paper sx={{ backgroundColor:'white' }} marginBottom={7.6}>
-      <Grid Container spacing={2}>
-        <Grid item xs={4}>
-        <Img
-              alt="menu_img"
-              src={drink}
-              sx={{width: '10%', backgroundColor: "#fafafa", margin: '0'}}
-            />
-        </Grid>
-        <Grid item xs={8} container direction="column">
-          <Grid item xs={8}>
-            <Typography>아이스 아메리카논</Typography>
-          </Grid>
-          <Grid item xs={4}>카/당/칼/가</Grid>
-        </Grid>
-      </Grid>
+    <Paper sx={{ backgroundColor: "white" }} marginBottom={7.6}>
+      <Paper>
+        {props.data.map((menu) => {
+          return (
+            <Grid Container key={menu.pk}>
+              <Grid item xs={4}>
+                <Img
+                  alt="menu_img"
+                  src={drink}
+                  sx={{ width: "10%", backgroundColor: "#fafafa", margin: "0" }}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <Grid item xs={8}>
+                  <Typography>{menu.name}</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                  {menu.caffeine} / {menu.sugar} / {menu.cal} / {menu.price}
+                </Grid>
+              </Grid>
+              <Divider/>
+            </Grid>
+          );
+        })}
+      </Paper>
     </Paper>
   );
 }
