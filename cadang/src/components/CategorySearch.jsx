2c762be1e1c303d848638ad8450c0e1dd0199f64
esom735 <esom735@gmail.com>
2023-02-03 10:26:00 +0900

    [FE] feat: 카페 리스트 Grid Item 디자인 수정

--- cadang/src/components/CategorySearch.jsx
+++ cadang/src/components/CategorySearch.jsx
@@ -10,7 +10,7 @@ const { kakao } = window;
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
