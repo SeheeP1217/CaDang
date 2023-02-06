fc0cc0322967299ebb174a0ec5ae48ad7e03326d
박세희 <park6938@naver.com>
2023-02-06 21:14:14 +0900

    feat: 주문내역페이지 생성

---
+++ FE/cadang/src/components/PaymentRecord.jsx
@@ -0,0 +1,14 @@
+import { Card, Grid } from "@mui/material"
+
+export default function PaymentRecord() {
+  return (
+    <div>
+      <Card>
+        <p>픽업완료</p>
+        <h4>매머드커피 역삼언주로점</h4>
+        <h3>아이스 아메리카노 1600원</h3>
+        <p>1월 25일</p>
+      </Card>
+    </div>
+  )
+}
--- FE/cadang/src/pages/ReportPages/PaymentReportPage.jsx
+++ FE/cadang/src/pages/ReportPages/PaymentReportPage.jsx
@@ -1,7 +1,26 @@
+import { Card, Grid, Button, Box } from "@mui/material"
+import PaymentRecord from "../../components/PaymentRecord"
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew"
+import { Link } from "react-router-dom"
+
 export default function PaymentReportPage() {
   return (
     <div>
-      주문 내역
+      <div>
+        <Grid container>
+          <Button component={Link} to="/mypage">
+            <ArrowBackIosNewIcon></ArrowBackIosNewIcon>
+          </Button>
+          <Box>주문 내역</Box>
+        </Grid>
+      </div>
+
+      <Card>
+        <Grid>
+          <PaymentRecord />
+          <PaymentRecord />
+        </Grid>
+      </Card>
     </div>
   )
-  }
+}
