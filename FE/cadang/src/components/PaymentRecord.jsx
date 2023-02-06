0c4c6c69f0accff2ca1d125497a6a6e9eb163389
박세희 <park6938@naver.com>
2023-02-06 21:43:15 +0900

    feat: 주문내역페이지 생성

--- FE/cadang/src/components/PaymentRecord.jsx
+++ FE/cadang/src/components/PaymentRecord.jsx
@@ -7,7 +7,7 @@ export default function PaymentRecord() {
         <p>픽업완료</p>
         <h4>매머드커피 역삼언주로점</h4>
         <h3>아이스 아메리카노 1600원</h3>
-        <p>1월 25일</p>
+        <p>2월 5일</p>
       </Card>
     </div>
   )
