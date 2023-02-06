e699f3d7d599bd4ff51c720b1d65c50a733bd420
esom735 <esom735@gmail.com>
2023-02-06 17:51:08 +0900

    [FE] feat: recoil 설정

--- FE/cadang/src/index.js
+++ FE/cadang/src/index.js
@@ -1,15 +1,19 @@
 import React from "react"
+import { RecoilRoot } from 'recoil';
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+
 
 import "./index.css"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RecoilRoot>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecoilRoot>
 )
 
 // ReactDOM.render(
