551d0f42b53f8f9cb1cd4f9068f0b47b80c66a59
esom735 <esom735@gmail.com>
2023-02-09 22:20:00 +0900

    [FE] feat: 카페 맵 DB에 있는 목록인지 확인 & 마커 css 적용

--- FE/cadang/src/api/main.jsx
+++ FE/cadang/src/api/main.jsx
@@ -6,7 +6,7 @@ const api = axios.create({
   baseURL: "http://i8a808.p.ssafy.io:8080",
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYmFiMTIzNCIsImlkIjoxOSwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTY3NTk4Nzc5NX0.B4bnh2yLtLC6Ps25ntRECNx2oXInlnksj9xL4_UWujMIQwn8SdMtAyD5Po_4AzLYDFjjHqE-Uefw0DgGt_xAYA",
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYmFiMTIzNCIsImlkIjoxOSwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTY3NTk5MDMxNn0.ZuHeaPY1IkSdQnDzbDu440xeMMU6OHr61KUHDj1aJyYAOOSnonG_udXtFDgvWGdbjOV2WCTSHq2elpimFqnurQ",
     "Content-Type": "application/json",
   },
 });
