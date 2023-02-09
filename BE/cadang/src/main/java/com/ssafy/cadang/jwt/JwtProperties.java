f6322778dbb25ac8a7f8c8d0ea5fbfff7fb7689e
정재영 <jaywjd@naver.com>
2023-02-09 10:54:25 +0900

    [BE]fix: userDto valid 수정

--- BE/cadang/src/main/java/com/ssafy/cadang/jwt/JwtProperties.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/jwt/JwtProperties.java
@@ -2,7 +2,9 @@ package com.ssafy.cadang.jwt;
 
 public interface JwtProperties {
     String SECRET = "amF5amF5LXZlbG9nLWp3dC1zcHJpbmctYm9vdC10cmFpbmluZy1zZWNyZXQta2V5LWpheWpheS1qd3QtdHJhaW5pbmctdHV0b3JpYWw=";
+    String REFRESH_SECRET = "Y2FkYW5nLXZlbG9nLWp3dC1zcHJpbmctYm9vdC10cmFpbmluZy1yZWZyZXNoLXNlY3JldC1rZXktand0LXJlZnJlc2gtdG9rZW4tZ2VuZXJhdGU=";
     int EXPIRATION_TIME = 60000 * 60 * 24; // 뒤에 숫자가 분
+    int REFRESH_EXPIRATION_TIME = 60000 * 60 * 24;
 
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
