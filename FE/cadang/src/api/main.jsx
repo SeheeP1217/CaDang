a4a496a4751c46268a6e7c2390d2f6f590062b50
esom735 <esom735@gmail.com>
2023-02-10 09:04:04 +0900

    [FE] main화면 토큰 갱신

--- FE/cadang/src/api/main.jsx
+++ FE/cadang/src/api/main.jsx
@@ -6,7 +6,7 @@ const api = axios.create({
   baseURL: "http://i8a808.p.ssafy.io:8080",
   headers: {
     Authorization:
-      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYmFiMTIzNCIsImlkIjoxOSwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTY3NTg0NTUyOH0.XWZ70Gf5gAjVZahqh4FnLzhVnXBUFrpI3FDAG6gNRChyk3v4Oshkw_jtwJWi-bDSooMvavz7YFl3wiJEvwlG5w",
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYmFiMTIzNCIsImlkIjoxOSwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTY3NjA3MzM2NX0.v7Wl5Q4_iLE7djrJ3OjkxO27lfticCWPPPQMSQLlzsxbI7517FR7FoISS68cG9lpOf5AULRZ7C985dtgelcMAw",
     "Content-Type": "application/json",
   },
 });
