19ad873d5d6e6f0d30cf17762ab4ccd2f3d30a2c
이윤경 <yxxnkxx@gmail.com>
2023-02-13 11:01:31 +0900

    [BE] file upload path 수정

--- BE/cadang/src/test/java/com/ssafy/cadang/service/DataServiceTest.java
+++ BE/cadang/src/test/java/com/ssafy/cadang/service/DataServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,6 +27,14 @@ class DataServiceTest {
     DataService dataService;
     @Autowired
     UserRepository userRepository;
+
+    @Test
+    public void dataCrate() {
+
+        for (int i = 1; i <= 31; i++)
+            dataService.createDataByRegDate(2L, LocalDate.of(2022, 12, i));
+
+    }
 
     @Test
     public void updateGoalTest() {
