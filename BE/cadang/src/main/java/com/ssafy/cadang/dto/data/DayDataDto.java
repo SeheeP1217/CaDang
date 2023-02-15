57f3ef4042eab87959b33de9da95dd7b60a959bb
이윤경 <yxxnkxx@gmail.com>
2023-02-15 17:43:49 +0900

    [BE] feat: userId

--- BE/cadang/src/main/java/com/ssafy/cadang/dto/data/DayDataDto.java
+++ BE/cadang/src/main/java/com/ssafy/cadang/dto/data/DayDataDto.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 @lombok.Data
 public class DayDataDto {
     private Long id;
+    private Long userId;
     private String nickname;
     private String image;
     private LocalDate date;
@@ -25,6 +26,7 @@ public class DayDataDto {
 
     public DayDataDto(Data data) {
         id = data.getId();
+        userId = data.getUser().getId();
         nickname = data.getUser().getNickname();
         image = "http://i8a808.p.ssafy.io/images/profile/" + data.getUser().getImgUrl();
         date = data.getRegDate();
